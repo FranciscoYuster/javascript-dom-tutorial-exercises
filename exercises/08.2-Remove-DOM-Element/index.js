3dff5e6ad3207e72bc682ac61b768565bbda89a7
Francisco Yuster <yusterfrancisco@gmail.com>
2025-01-21 22:28:45 +0000

    DOM

--- exercises/08.2-Remove-DOM-Element/index.js
+++ exercises/08.2-Remove-DOM-Element/index.js
@@ -1,1 +1,4 @@
 // Your code here
+let list = document.querySelector('#parentLi');
+let childs = list.childNodes[3];
+list.removeChild(childs);
--- exercises/10-Add-li-on-Click/index.js
+++ exercises/10-Add-li-on-Click/index.js
@@ -1,5 +1,8 @@
 let button = document.getElementById("superDuperButton");
 button.addEventListener("click", function() {
 	// Your code here
-
+	let cli = document.createElement("li");
+	cli.innerHTML = "NewElement";
+	let myList = document.getElementById("myList");
+	myList.appendChild(cli);
 });
